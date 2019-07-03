d21c350515b3d60f8449befb7c55a5306fb81d1d
Mihailescu Marian <mariandev96@gmail.com>
2019-07-03 18:12:58 +0200

    chore: Changed the libraryTarget to UMD

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 	output: {
 		filename: 'template-blocks.js',
 		path: path.resolve(__dirname, "dist"),
-		libraryTarget: "window"
+		libraryTarget: "umd"
 	},
 	plugins: [
 		new DtsBundlePlugin(),
