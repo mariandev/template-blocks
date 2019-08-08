dadf79272b8f1e1463b8cca1b774d98d8aa275da
Mihailescu Marian <mariandev96@gmail.com>
2019-08-08 17:49:57 +0300

    feat: Added support for processors with 3 parameters

--- src/ValueProcessors/Base/GenericValueProcessor.ts
+++ src/ValueProcessors/Base/GenericValueProcessor.ts
@@ -1,11 +1,12 @@
 import {PropsVariants, ValueProcessor} from "./ValueProcessor";
 
-export class GenericValueProcessor<TOut, TP1, TP2> extends ValueProcessor<TOut, TP1, TP2> {
+export class GenericValueProcessor<TOut, TP1, TP2, TP3> extends ValueProcessor<TOut, TP1, TP2, TP3> {
 
 	constructor(implementation: () => TOut);
 	constructor(implementation: (arg1: TP1) => TOut, arg1: TP1);
 	constructor(implementation: (arg1: TP1, arg2: TP2) => TOut, arg1: TP1, arg2: TP2);
-	constructor(public readonly implementation: (...args: any[]) => TOut, ...args: PropsVariants<TP1, TP2>) {
+	constructor(implementation: (arg1: TP1, arg2: TP2, arg3: TP3) => TOut, arg1: TP1, arg2: TP2, arg3: TP3);
+	constructor(public readonly implementation: (...args: any[]) => TOut, ...args: PropsVariants<TP1, TP2, TP3>) {
 		super(...args as []);
 	}
 
@@ -13,9 +14,9 @@ export class GenericValueProcessor<TOut, TP1, TP2> extends ValueProcessor<TOut, 
 		return this.implementation(...this.params);
 	}
 
-	static new<TOut, TP1, TP2>(implementation: (...args: PropsVariants<TP1, TP2>) => TOut) {
-		return class Anonymous extends GenericValueProcessor<TOut, TP1, TP2> {
-			constructor(...args: PropsVariants<TP1, TP2>) {
+	static new<TOut, TP1, TP2, TP3>(implementation: (...args: PropsVariants<TP1, TP2, TP3>) => TOut) {
+		return class Anonymous extends GenericValueProcessor<TOut, TP1, TP2, TP3> {
+			constructor(...args: PropsVariants<TP1, TP2, TP3>) {
 				super(implementation, ...args as []);
 			}
 		};
